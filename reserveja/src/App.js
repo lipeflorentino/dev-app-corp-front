ab23ef16e7344e46490ea21206197b7d98ab7b6f
lipeflorentino <lipeflorentino2@gmail.com>
2019-07-21 18:00:48 -0300

    subindo alteracoes

---
+++ reserveja/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ReserveJá</h1>        
+        
+      </header>
+
+      <div>
+      <h1>ReserveJá</h1>   
+      </div>
+    </div>
+  );
+}
+
+export default App;
